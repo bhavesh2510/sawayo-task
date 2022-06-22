3114ce77fb557a201505f4377d8e1564bf08674f
Bhavesh singh <bhavesh@zotto.io>
2022-06-22 15:01:48 +0530

    submit test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 function App() {
+  const [data, setData] = useState({ values: [] })
+
+  const getData = async () => {
+    axios
+      .get("https://api.github.com/users?since=1&per_page=100")
+      .then((response) => {
+        response.data?.map((currentVal) => {
+          setData((prev) => ({
+            values: [...prev.values, currentVal.login]
+          }))
+        })
+      })
+  }
+  useEffect(() => {
+    getData()
+  }, [])
+
+  const [SearchValue, setSearchValue] = useState()
+
+  const showSuggestion = async (e) => {
+    setSearchValue()
+    if (!e.target.value) {
+      await getData()
+    }
+    const filteredSuggestions = data.values?.filter(
+      (suggestion) => suggestion.toLowerCase().indexOf(e.target.value) > -1
+    )
+    console.log("filtered sugg", filteredSuggestions, e.target.value)
+    setData({ values: filteredSuggestions })
+    console.log(filteredSuggestions)
+  }
+
+  const Suggestions = () => {
+    return (
+      <ul className='suggestions'>
+        {data.values?.map((suggestion, index) => {
+          return (
+            <li
+              // className={index === suggestionIndex ? "active" : ""}
+              onClick={() => {
+                setSearchValue(suggestion)
+                setData({ values: [] })
+              }}
+              style={{ cursor: "pointer" }}
+              key={index}
+              // onClick={handleClick}
+            >
+              {suggestion}
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
+
+  useEffect(() => {
+    console.log(SearchValue)
+  }, [SearchValue])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input type='text' value={SearchValue} onChange={showSuggestion} />
+      <Suggestions />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
